f1c159dfa2475ce28c0c3f7333802b39c89aa0fb
unknown <athuldascr7@gmail.com>
2022-11-18 15:36:19 +0530

    calculator

---
+++ calc.js
@@ -0,0 +1,18 @@
+function display(num){
+    result.value+=num
+    // result.value=result.value+num
+
+           
+}
+
+function allclear(){
+    result.value=""
+}
+
+function evaluateexp(){
+    result.value=eval(result.value)
+}
+
+function backspace(){
+    result.value=result.value.slice(0,-1)
+}
